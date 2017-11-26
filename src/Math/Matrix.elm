2ba481fec670133301840c1d7a91862e507cd565
Rogerio Chaves <rogeriocfj@gmail.com>
2017-11-26 12:16:31 -0200

    Migrate Math.Matrix to use numelm

--- src/Math/Matrix.elm
+++ src/Math/Matrix.elm
@@ -1,109 +1,96 @@
 module Math.Matrix exposing (..)
 
-import Helpers exposing (maybeFlatMap, unwrapMaybe)
+import Helpers exposing (flatResultList, maybeFlatMap, unwrap, unwrapMaybe)
 import List.Extra
-import Math.Vector exposing (..)
+import NumElm exposing (..)
 
 
-type alias Matrix a =
-    List (List a)
-
-
-height : Matrix a -> Int
+height : NdArray -> Int
 height matrix =
-    List.length matrix
+    NumElm.size matrix
+        |> List.head
+        |> Maybe.withDefault 0
 
 
-width : Matrix a -> Int
+width : NdArray -> Int
 width matrix =
-    List.head matrix
-        |> Maybe.withDefault []
-        |> List.length
+    NumElm.size matrix
+        |> List.tail
+        |> Maybe.andThen List.head
+        |> Maybe.withDefault 0
 
 
-size : Matrix a -> ( Int, Int )
-size matrix =
-    ( height matrix, width matrix )
+transpose : NdArray -> NdArray
+transpose matrix =
+    NumElm.transpose matrix
+        |> toList
+        |> Result.andThen (NumElm.matrix Float64)
+        |> unwrap
 
 
-transpose : Matrix a -> Matrix a
-transpose =
-    List.Extra.transpose
+getColumn : Int -> NdArray -> Maybe NdArray
+getColumn n matrix =
+    NumElm.slice [ 0, n ] [ height matrix, n + 1 ] matrix
 
 
-getRow : Int -> Matrix a -> Maybe (Vector a)
-getRow =
-    List.Extra.getAt
+getAt : Int -> Int -> NdArray -> Maybe number
+getAt i j =
+    NumElm.get [ i, j ]
 
 
-getColumn : Int -> Matrix a -> Maybe (Vector a)
-getColumn n =
-    List.Extra.getAt n << transpose
-
-
-getAt : Int -> Int -> Matrix a -> Maybe a
-getAt i j matrix =
-    getRow i matrix
-        |> Maybe.andThen (List.Extra.getAt j)
-
-
-getColumns : List Int -> Matrix a -> Maybe (Matrix a)
+getColumns : List Int -> NdArray -> Maybe NdArray
 getColumns ns matrix =
     let
-        transMatrix =
-            transpose matrix
+        concatColumns columns =
+            let
+                tail =
+                    List.tail columns |> Maybe.withDefault []
+            in
+            List.head columns
+                |> Maybe.andThen (tailHeadConcat tail)
+
+        tailHeadConcat tail head =
+            List.foldl
+                (flip (NumElm.concatAxis 1) >> Result.andThen)
+                (Ok head)
+                tail
+                |> Result.toMaybe
     in
-    maybeFlatMap (\n -> List.Extra.getAt n transMatrix) ns
-        |> Maybe.map transpose
+    maybeFlatMap (flip getColumn matrix) ns
+        |> Maybe.andThen concatColumns
 
 
-multiplyVector : Matrix number -> Vector number -> Vector number
-multiplyVector matrix vector =
-    let
-        multiplyRow row result =
-            result ++ [ List.sum (List.map2 (*) vector row) ]
-    in
-    List.foldl multiplyRow [] matrix
+multiplyVector : NdArray -> NdArray -> Result String NdArray
+multiplyVector =
+    multiply
 
 
-multiply : Matrix number -> Matrix number -> Result String (Matrix number)
-multiply a b =
-    let
-        bTrans =
-            transpose b
-
-        multiplyRow row result =
-            result ++ [ multiplyVector bTrans row ]
-    in
-    if width a /= height b then
-        Err <|
-            "Matrix sizes are not fit for multiplication: Could not multiply a "
-                ++ toString (size a)
-                ++ " matrix with a "
-                ++ toString (size b)
-                ++ " matrix"
-    else
-        Ok (List.foldl multiplyRow [] a)
+multiply : NdArray -> NdArray -> Result String NdArray
+multiply =
+    NumElm.dot
 
 
-prependColumn : Vector a -> Matrix a -> Result String (Matrix a)
-prependColumn column matrix =
-    if List.length column /= height matrix then
-        Err <|
-            "Could not prepend a "
-                ++ toString (List.length column)
-                ++ "-sized vector to a "
-                ++ toString (size matrix)
-                ++ " matrix"
-    else
-        Ok <| transpose (column :: transpose matrix)
+prependColumn : NdArray -> NdArray -> Result String NdArray
+prependColumn =
+    NumElm.concatAxis 1
 
 
-unsafeGetColumn : Int -> Matrix a -> Vector a
+unsafeGetColumn : Int -> NdArray -> NdArray
 unsafeGetColumn n =
-    getColumn n >> unwrapMaybe ("could not get column " ++ toString n)
+    getColumn n >> unwrapMaybe ("could not get column " ++ Basics.toString n)
 
 
-unsafeGetColumns : List Int -> Matrix a -> Matrix a
+unsafeGetColumns : List Int -> NdArray -> NdArray
 unsafeGetColumns ns =
-    getColumns ns >> unwrapMaybe ("could not get columns " ++ toString ns)
+    getColumns ns >> unwrapMaybe ("could not get columns " ++ Basics.toString ns)
+
+
+toList : NdArray -> Result String (List (List Float))
+toList matrix =
+    NumElm.dataToString matrix
+        |> String.filter ((/=) ']')
+        |> String.filter ((/=) '[')
+        |> String.split ","
+        |> List.map String.toFloat
+        |> flatResultList
+        |> Result.map (List.Extra.groupsOf <| width matrix)
--- tests/Math/MatrixTest.elm
+++ tests/Math/MatrixTest.elm
@@ -2,7 +2,9 @@ module Math.MatrixTest exposing (..)
 
 import ElmTestBDDStyle exposing (..)
 import Expect exposing (..)
+import Helpers exposing (unwrap)
 import Math.Matrix exposing (..)
+import NumElm exposing (Dtype(Float64), NdArray, matrix, vector)
 import Test exposing (..)
 
 
@@ -13,26 +15,39 @@ import Test exposing (..)
 suite : Test
 suite =
     describe "Math.Matrix"
-        [ it "transposes square matrixes" <|
-            expect (transpose sample2x2)
+        [ it "converts a matrix to list" <|
+            expect (sample2x2 |> toList)
                 to
                 equal
-                [ [ 1, 3 ]
-                , [ 2, 4 ]
-                ]
+                (Ok
+                    [ [ 1, 2 ]
+                    , [ 3, 4 ]
+                    ]
+                )
+        , it "transposes square matrixes" <|
+            expect (transpose sample2x2 |> Ok)
+                to
+                equal
+                (matrix Float64
+                    [ [ 1, 3 ]
+                    , [ 2, 4 ]
+                    ]
+                )
         , it "transposes non-squared matrixes" <|
-            expect (transpose sample2x3)
+            expect (transpose sample2x3 |> Ok)
                 to
                 equal
-                [ [ 1, 4 ]
-                , [ 2, 5 ]
-                , [ 3, 6 ]
-                ]
+                (matrix Float64
+                    [ [ 1, 4 ]
+                    , [ 2, 5 ]
+                    , [ 3, 6 ]
+                    ]
+                )
         , it "multiplies matrixes 2x2 and 2x3" <|
             expect (multiply sample2x2 sample2x3)
                 to
                 equal
-                (Ok
+                (matrix Float64
                     [ [ 9, 12, 15 ]
                     , [ 19, 26, 33 ]
                     ]
@@ -41,7 +56,7 @@ suite =
             expect (multiply sample2x3 (transpose sample2x3))
                 to
                 equal
-                (Ok
+                (matrix Float64
                     [ [ 14, 32 ]
                     , [ 32, 77 ]
                     ]
@@ -50,41 +65,63 @@ suite =
             expect (multiply sample2x3 sample2x3)
                 to
                 equal
-                (Err "Matrix sizes are not fit for multiplication: Could not multiply a (2,3) matrix with a (2,3) matrix")
+                (Err "NdArray#dot - Incompatible shapes: The shape of nda1 is 2×3, but nda2 says 2×3")
         , it "prepends a column" <|
-            expect (prependColumn [ 0, 0 ] sample2x3)
+            expect (prependColumn (vector Float64 [ 0, 0 ] |> unwrap) sample2x3)
                 to
                 equal
-                (Ok
+                (matrix Float64
                     [ [ 0, 1, 2, 3 ]
                     , [ 0, 4, 5, 6 ]
                     ]
                 )
         , it "does prepends a column with different size" <|
-            expect (prependColumn [ 0, 0, 0 ] sample2x3)
+            expect (prependColumn (vector Float64 [ 0, 0, 0 ] |> unwrap) sample2x3)
                 to
                 equal
-                (Err "Could not prepend a 3-sized vector to a (2,3) matrix")
+                (Err "NdArray#concat - Incompatible shapes: The shape of nda1 is 3×1, but nda2 says 2×3 on axis 1")
         , it "returns correct width" <|
             expect (width sample2x3) to equal 3
         , it "returns correct height" <|
             expect (height sample2x3) to equal 2
-        , it "returns correct size" <|
-            expect (size sample2x3) to equal ( 2, 3 )
-        , it "gets a row" <|
-            expect (getRow 1 sample2x3) to equal (Just [ 4, 5, 6 ])
+        , it "gets a value" <|
+            expect (getAt 1 2 sample2x3) to equal (Just 6)
+        , it "get a column" <|
+            expect (getColumn 1 sample2x3)
+                to
+                equal
+                (vector Float64 [ 2, 5 ] |> Result.toMaybe)
+        , it "get two columns" <|
+            expect (getColumns [ 0, 2 ] sample2x3)
+                to
+                equal
+                (matrix Float64
+                    [ [ 1, 3 ]
+                    , [ 4, 6 ]
+                    ]
+                    |> Result.toMaybe
+                )
+        , it "multiplies by a vector" <|
+            expect (multiplyVector sample2x3 (NumElm.vector Float64 [ 2, 3, 5 ] |> unwrap))
+                to
+                equal
+                (vector Float64 [ 23, 53 ])
         ]
 
 
-sample2x2 : Matrix number
+sample2x2 : NdArray
 sample2x2 =
-    [ [ 1, 2 ]
-    , [ 3, 4 ]
-    ]
+    matrix Float64
+        [ [ 1, 2 ]
+        , [ 3, 4 ]
+        ]
+        |> unwrap
 
 
-sample2x3 : Matrix number
+sample2x3 : NdArray
 sample2x3 =
-    [ [ 1, 2, 3 ]
-    , [ 4, 5, 6 ]
-    ]
+    matrix Float64
+        [ [ 1, 2, 3 ]
+        , [ 4, 5, 6 ]
+        ]
+        |> unwrap
